f9a42f019ce6eac040a7e745572e328f8ed91b29
a.van.hensbergen <a.van.hensbergen@rocmondriaan.nl>
2022-03-12 22:22:28 +0100

    finished flippableTangramPiece

--- app/models/TangramPiece.py
+++ app/models/TangramPiece.py
@@ -12,35 +12,30 @@ class TangramPiece:
       __resetCoords = None
       
       def __init__(self, canvas : Canvas, *coords,**options):
-        self.__canvas = canvas
-        self.__resetCoords =coords
-        self.__id = self.__canvas.create_polygon(coords, options);
-        count = 0
-        numberOfCorners = len(coords)/2
-        for coord in coords:
-            if count%2 ==  0:
-                  self._X += coord
-            else:
-                  self._Y+= coord
-            count +=1
-        self._X = self._X/numberOfCorners
-        self._Y = self._Y/numberOfCorners
-        self.bind("<Motion>", self.onDrag);
-        self.bind('<ButtonPress>', self.onDragStart )
-        self.bind('<ButtonRelease>', self.onDragEnd)
-        self.bind('<Double-Button>', self.onDoubleClick)
-        #self.bind('<Leave>',lambda event: print("test"))
+            self.__canvas = canvas
+            self.__resetCoords =coords
+            self.__id = self.__canvas.create_polygon(coords, options);
+            self.setCenter(coords)
+            self.bind("<Motion>", self.onDrag);
+            self.bind('<ButtonPress>', self.onDragStart )
+            self.bind('<ButtonRelease>', self.onDragEnd)
+            self.bind('<Double-Button>', self.onDoubleClick)
         
       def onDoubleClick(self,event):
             coords = self.coords()
             newcoords = self.rotate(45,*coords) 
             self.update(newcoords)
             
+           
       """
       description of onDragStart(self,event)
       the method is a handler for ButtonPress
       it sets a number of properties of the tangrampiece nessecary for 
-      updating when being dragged.
+      updating when being dragged to be specific.
+      dragStartX and dragStartY are the position where the drag started. These are used to determine the 
+      distance moved by the mouse
+      dragStartCoords are the coordinates of the polygone at start drag
+      once the distance moved is known it can be added to the dragStartCoords to find nwew coords for the polygone
       :event is the event that is generated and recieved by the handler
       the method sets the mousepoition on dragstart and sets the cornercoordinates of the piece.
       """  
@@ -48,6 +43,7 @@ class TangramPiece:
             self._dragStartX = event.x
             self._dragStartY = event.y
             self._dragStartCoords = self.coords()
+      
       
       """
       the description of onDragEnd(self, event)
@@ -63,41 +59,53 @@ class TangramPiece:
       description of onDrag(self,event); it is a handler for draghandling 
       :event is the event that is generated and recieved by the handler
       the method determines new coords for the corners of the tangrampiece and 
-      determines the new center of mass of the systenm ie its self.__X and self.__Y
-      after that it updates the tangrampiece
+      determines the new center of mass of the systenm ie its self._X and self._Y
+      after that it updates the tangrampiece polygone on the canvas
       """  
       def onDrag(self,event):
             if self.dragStarted:
                   deltaX = event.x - self._dragStartX
                   deltaY = event.y - self._dragStartY
-                  numberOfCorners = len(self._dragStartCoords)
-                  newCoords = self._dragStartCoords.copy()
-                  self._X =0
-                  self._Y =0
-                  for i in range(0,numberOfCorners):
-                        if i %2 == 0:
-                              newCoords[i] += deltaX
-                              self._X += newCoords[i]
-                        else:
-                              newCoords[i] += deltaY
-                              self._Y += newCoords[i]
-                              
-                  self._X = 2*self._X /numberOfCorners
-                  self._Y = 2*self._Y /numberOfCorners
+                  newCoords = self.translate(deltaX, deltaY,self._dragStartCoords)
+                  self.setCenter(newCoords)
                   self.update(newCoords)
                   
-                  
+      def translate(self, deltaX, deltaY, coords):
+            
+            newCoords = []
+            count =0
+            for coord in coords:
+                  if count%2 == 0:
+                        newCoords.append(coord + deltaX)
+                  else:
+                        newCoords.append(coord + deltaY)
+                  count +=1
+            return newCoords
+      
+      def setCenter(self, coords):
+            self._X = 0
+            self._Y = 0
+            count = 0
+            numberOfCorners = len(coords)/2
+            for coord in coords:
+                  if count%2 ==  0:
+                        self._X += coord
+                  else:
+                        self._Y+= coord
+                  count +=1
+            self._X = self._X/numberOfCorners
+            self._Y = self._Y/numberOfCorners
+            
       """
             description of rotate(self,angle,*coords)
             this method will rotate the tangrampiece over a given amount of degrees
-            rotation direction is counterclockwise. Rotation is performed round X and Y 
+            rotation direction is counterclockwise. Rotation is performed round _X and _Y 
             being the center of the Tangrampiece
             :angle in degrees
             *coords the coordinates as a list of the polygone
             returns the rotated coords of the polygone
       """         
       def rotate(self,angle,*coords):
-            # coords = self.coords()
             newcoords = []
             cosAngle = cos(angle*pi/180)
             sinAngle = sin(angle*pi/180)
@@ -118,6 +126,7 @@ class TangramPiece:
       """      
       def update(self, coords):
             self.canvas.coords(self.id,coords)
+            self.__canvas.tkraise(self.id)
       
       """
       description of coords(self)
